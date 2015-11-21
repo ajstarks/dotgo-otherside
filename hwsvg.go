a2797b20a3bb9c58c1dc6cdda43d8a30b1ab7bce
Anthony Starks <ajstarks@gmail.com>
2015-11-21 15:35:56 -0500

    revamp SVGo hello, world

--- hwsvg.go
+++ hwsvg.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-    "os"
-    "github.com/ajstarks/svgo"
+	"github.com/ajstarks/svgo"
+	"os"
 )
 
 func main() {
-    width := 960
-    height := 540
-    canvas := svg.New(os.Stdout)
-    canvas.Start(width, height)
-    canvas.Rect(0, 0, width, height, "fill:black")
-    canvas.Circle(width/2, height, width/2, "fill:rgb(44,77,232)")
-    canvas.Text(width/2, height/2, "hello, world",
-        "fill:white;font-size:60pt;font-family:serif;text-anchor:middle")
-    canvas.End()
+	width := 960
+	height := 540
+	style := "fill:white;font-size:60pt;font-family:serif;text-anchor:middle"
+	canvas := svg.New(os.Stdout)
+	canvas.Start(width, height)
+	canvas.Rect(0, 0, width, height, "fill:black")
+	canvas.Circle(width/2, height, width/2, "fill:rgb(44,77,232)")
+	canvas.Text(width/2, height/2, "hello, world", style)
+	canvas.End()
 }
